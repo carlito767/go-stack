effa7c6a5453142fd9418d6053688d929a6042a6
carlito767 <charles.longuemart@gmail.com>
2023-10-03 02:04:55 +0200

    chore: add a short description for Recoverer

--- middleware/recoverer.go
+++ middleware/recoverer.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Recoverer is a middleware that recovers from panics that occur during the execution of a request.
 func Recoverer(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
